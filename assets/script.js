5578c0c392a8dc8b7c41d6758df697dfcdd3f7ff
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-10-01 01:21:35 -0500

    linked style sheet and js script added working time and date momment function.

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// var unixFormat = moment.unix(1318781876).format("MMM Do, YYYY, hh:mm:ss");
+// $("#6a").text(unixFormat);
+var today = moment();
+$("#1a").text(today.format("[Today is ] MMM Do, YYYY, h:mm:ss A"));
+
+// var dayWeek = today.format("[Today is] MMM DO, YYYY");
+// $("#6a").text(dayWeek);
