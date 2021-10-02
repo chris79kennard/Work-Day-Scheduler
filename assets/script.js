e7eebdfda5d89dd6108705f686354d02e6821c6a
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-10-01 21:03:57 -0500

    layed out psudeo code

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,71 @@
 var today = moment();
-$("#time").text(today.format("[Today is ] MMM Do, YYYY, h:mm:ss A"));
+$("#time").text(today.format("[Today is ] MMM Do, YYYY, h:mm A"));
+// grab the div with class of container and store in variable
+
+// grab current date p tag and stor in variable
+
+// grab the reset button
+
+// crate global variables
+
+// define a function that calls checkTime and getCurrentTime functions
+
+// use moment.js to generate the current date
+
+// set text of currentDate element to the date
+
+// use moment.js to generate the current time
+
+// conditionally apply colors
+
+// use setInterval to fetch the current data and time every second
+
+// iterate 24 times to create rows of data with 3 total columns
+
+// create div for the time box rows
+
+// create div to hold hour display span tag and create span tag
+
+// create input text box
+
+// create div to hold save button and create button element
+
+// create save button icon element and add font awesome class for icon
+
+// append timeBox to the calendar
+
+// append hour span to div and append div to timeBox
+
+// use conditional logic to set the correct time with AM or PM
+
+// append save button div to timeBox and append button to div
+
+// add event listener on each button and input
+
+// create function for when the button is clicked
+
+// create variable to store the info input from the textarea
+
+// store textAreaText value in local storage with unique keys
+
+// create a function that can be used to extract memory when page is reloaded or revisited
+
+// loop through the time boxes
+
+// call the function to extract and display previously saved
+
+// create function that will reset the values of all of the text boxes
+
+// add event listener for reset button
+
+//// when planner is opened, the current day is displayed at the top of the calendar
+
+//// calendar needs time blocks (rows) for standard business hours
+
+//// time blocks are color coordinated to indicate the past (gray), the present (orange), and the future (turquoise)
+
+//// when time block (row) is clicked, the user can enter text
+
+// when the save button is clicked, the text for that input is saved to local storage and text input becomes
+
+// when page is refreshed, the saved text is still in the time block
